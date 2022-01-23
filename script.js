1f62aaa1c9a27aca4553fceb0b5dd08835e1298b
Yash Gautam <98219582+iTrydant@users.noreply.github.com>
2022-01-23 07:27:21 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let num = Number(prompt('Please enter an number of which you want to find the Square root!'))
+
+switch (num) {
+  default:
+    console.log('Please enter an number!')
+}
+
+let squareRoot = Math.sqrt(num);
+document.write(squareRoot)
